2da061801106466eb6a14417d607a794d2c6e373
Onsen UI <onsen03@gmail.com>
2018-02-05 21:09:39 +0900

    add type declarations

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -1,10 +1,12 @@
 module Main where
 
-import Prelude ((+), (*))
-import Control.Monad.Eff.Console (logShow)
+import Prelude (Unit, (+), (*))
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
 import Math (sqrt)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = logShow (diagonal 3.0 4.0)
