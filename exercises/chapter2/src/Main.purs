5c7120c66de24c61e4fdfaa06d60f9ee23c383c5
Onsen UI <onsen03@gmail.com>
2018-02-05 21:11:51 +0900

    add circleArea

--- exercises/chapter2/src/Main.purs
+++ exercises/chapter2/src/Main.purs
@@ -3,10 +3,13 @@ module Main where
 import Prelude (Unit, (+), (*))
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, logShow)
-import Math (sqrt)
+import Math (sqrt, pi)
 
 diagonal :: Number -> Number -> Number
 diagonal w h = sqrt (w * w + h * h)
 
+circleArea :: Number -> Number
+circleArea r = pi * r * r
+
 main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = logShow (diagonal 3.0 4.0)
+main = logShow (circleArea (diagonal 3.0 4.0))
