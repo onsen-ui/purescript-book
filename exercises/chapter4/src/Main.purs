be1f09fb3c9f56ded2348bff7f6e99dcde143e2d
Onsen UI <onsen03@gmail.com>
2018-02-07 03:21:39 +0900

    add fact, fib, length

---
+++ exercises/chapter4/src/Main.purs
@@ -0,0 +1,26 @@
+module Main where
+
+import Prelude
+import Data.Array (null)
+import Data.Array.Partial (tail)
+import Partial.Unsafe (unsafePartial)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Console (CONSOLE, logShow)
+
+fact :: Int -> Int
+fact 0 = 1
+fact n = n * fact(n - 1)
+
+fib :: Int -> Int
+fib 0 = 1
+fib 1 = 1
+fib n = fib(n - 1) + fib(n - 2)
+
+length :: forall a. Array a -> Int
+length arr =
+  if null arr
+    then 0
+    else 1 + length(unsafePartial tail arr)
+
+main :: Eff (console :: CONSOLE) Unit
+main = logShow "Hello"
