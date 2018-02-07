8dda711f11e55e533360ccac0b67519b009060bc
Onsen UI <onsen03@gmail.com>
2018-02-08 07:11:55 +0900

    refactor NonEmptyArray

--- exercises/chapter6/src/NonEmptyArray.purs
+++ exercises/chapter6/src/NonEmptyArray.purs
@@ -3,14 +3,17 @@ module NonEmptyArray where
 import Prelude
 
 import Data.Array ((:))
-import Data.Maybe (Maybe(..))
 
 data NonEmpty a = NonEmpty a (Array a)
+
 instance showNonEmpty :: Show a => Show (NonEmpty a) where
   show (NonEmpty x xs) = "NonEmpty " <> show (x:xs)
+
 instance eqNonEmpty :: Eq a => Eq (NonEmpty a) where
   eq (NonEmpty x xs) (NonEmpty y ys) = x == y && xs == ys
+
 instance semigroupNonEmpty :: Semigroup (NonEmpty a) where
   append (NonEmpty x xs) (NonEmpty y ys) = NonEmpty x (xs <> [y] <> ys)
+
 instance functorNonEmpty :: Functor NonEmpty where
   map f (NonEmpty x xs) = NonEmpty (f x) (f <$> xs)
