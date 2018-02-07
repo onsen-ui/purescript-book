f5293d1ca6a92a658cc23e36a14a46a9244fe7c7
Onsen UI <onsen03@gmail.com>
2018-02-07 09:41:32 +0900

    add fromString, toString

--- exercises/chapter5/src/Data/Main.purs
+++ exercises/chapter5/src/Data/Main.purs
@@ -8,3 +8,11 @@ gcd' 0 m = m
 gcd' n m = if n > m
   then gcd' (n - m) m
   else gcd' n (m - n)
+
+fromString :: String -> Boolean
+fromString "true" = true
+fromString _      = false
+
+toString :: Boolean -> String
+toString true = "true"
+toString false = "false"
