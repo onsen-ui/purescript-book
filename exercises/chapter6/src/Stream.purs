9439578e5019caf28a292a2ccfa84ab1598a3312
Onsen UI <onsen03@gmail.com>
2018-02-08 20:42:17 +0900

    add Stream

---
+++ exercises/chapter6/src/Stream.purs
@@ -0,0 +1,25 @@
+module Stream where
+
+import Prelude
+
+import Data.Array as Array
+import Data.Maybe (Maybe(..))
+import Data.Monoid (class Monoid, mempty)
+import Data.String as String
+
+class Stream stream element | stream -> element where
+  uncons :: stream -> Maybe { head :: element, tail :: stream }
+
+instance streamArray :: Stream (Array a) a where
+  uncons = Array.uncons
+
+instance streamString :: Stream String Char where
+  uncons = String.uncons
+
+foldStream :: forall l e m. Stream l e => Monoid m => (e -> m) -> l -> m
+foldStream f list =
+  case uncons list of
+    Nothing -> mempty
+    Just cons -> f cons.head <> foldStream f cons.tail
+
+genericTail xs = map _.tail (uncons xs)
